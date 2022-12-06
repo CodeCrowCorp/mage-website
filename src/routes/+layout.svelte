4640133665bba1fbea9163bb9dcdecf7f4904f9c
Gagan Suie <gagan.suie@codecrow.io>
2022-12-06 17:40:47 -0600

    Feat: added placeholder routes, stores, and most assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,32 @@
-<script>
-  import "../app.css";
+<script lang="ts">
+	import '../app.css'
+
+	export async function load({ page, session }: { page: any; session: any }) {
+		const isAdminPage = /^\/admin\/(.*)/.test(page.path)
+		const isHomePage = /^\/home\/(.*)/.test(page.path)
+		const isProfilePage = /^\/profile\/(.*)/.test(page.path)
+		const isPremiumPage = /^\/premium\/(.*)/.test(page.path)
+		const isChannelDetailsPage = /^\/channel\/(.*)/.test(page.path)
+		const isMaintenanceModeEnabled = true
+		if (
+			!session.user &&
+			(isAdminPage || isHomePage || isProfilePage || isPremiumPage || isChannelDetailsPage)
+		) {
+			return {
+				status: 302,
+				redirect: '/login'
+			}
+		} else {
+			if (isMaintenanceModeEnabled) {
+				if (!session.user.isAdmin) {
+					return {
+						status: 302,
+						redirect: '/maintenance'
+					}
+				} else return { props: {} }
+			} else return { props: {} }
+		}
+	}
 </script>
 
 <slot />
--- src/stores/channelStore.ts
+++ src/stores/channelStore.ts
@@ -1,12 +1,14 @@
 import { derived, writable, type Writable } from "svelte/store"
+import { env } from '$env/dynamic/public'
 
 class ChannelStore {
     constructor(
         public firstname: Writable<string> = writable(''),
         public lastname: Writable<string> = writable(''),
-    ) { }
+    ) {}
 
     get fullname() {
+        console.log(env.PUBLIC_API_KEY) // public
         // Use derived to access writable values and export as readonly
         return derived(
             [this.firstname, this.lastname],
