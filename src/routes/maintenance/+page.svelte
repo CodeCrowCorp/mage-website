648282f516f4a2711244e7acdc2f50ad9a699d31
Gagan Suie <gagan.suie@codecrow.io>
2022-12-31 20:45:03 +0000

    Fix: broken themeChange and added minor pages

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,1 +1,24 @@
-<h1>Welcome to maintenance page</h1>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+            <div class="flex justify-center">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-12 h-12">
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+				</svg>
+			</div>
+			<p class="py-6">
+				We are performing maintenance on the app. We will be back up shortly. We appreciate your
+				patience!
+			</p>
+		</div>
+	</div>
+</div>
