e7ff30fca30d46e50a468f5da08781db9e14a8fa
Gagan Suie <gagan.suie@codecrow.io>
2023-01-01 07:57:37 +0000

    Feat: added tab name and urls to legal page server

--- src/routes/legal/+page.server.ts
+++ src/routes/legal/+page.server.ts
@@ -3,11 +3,18 @@ import type { PageServerLoad } from './$types'
 import { adminStore } from '../../lib/stores/adminStore'
 
 export const load = (async () => {
-	// const post = null; //await adminStore.getLegalDocs()
-
-	// if (post) {
-	// 	return post
-	// }
-
-	// throw error(404, 'Not found')
+	//const post = await adminStore.getLegalDocs()
+	const post = [
+		{ name: "Privacy Policy", url: "https://code-crow-legal-docs.s3.amazonaws.com/legal/privacy.pdf" },
+		{ name: "Cookie Policy", url: "https://code-crow-legal-docs.s3.amazonaws.com/legal/cookie.pdf" },
+		{ name: "Copyright Policy", url: "https://code-crow-legal-docs.s3.amazonaws.com/legal/copyright.pdf" },
+		{ name: "GDPR Policy", url: "https://code-crow-legal-docs.s3.amazonaws.com/legal/gdpr.pdf" },
+		{ name: "Terms of Service", url: "https://code-crow-legal-docs.s3.amazonaws.com/legal/terms.pdf" }
+	]
+	if (post) {
+		return {
+			post
+		}
+	}
+	throw error(404, 'Not found')
 }) satisfies PageServerLoad
--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,18 +1,13 @@
-<script>
-	let tabs = [
-		"Privacy Policy",
-		"Cookie Policy",
-		"Copyright Policy",
-		"GDPR Policy",
-		"Terms of Service"
-	]
+<script lang="ts">
+	import type { PageData } from './$types'
+	export let data: PageData
 	let activeTab = 0
 </script>
 <div class="h-screen flex justify-center flex-col">
 	<div class="tabs tabs-boxed flex justify-center">
-		{#each tabs as tab, index}
-		  <button class="tab" class:tab-active={activeTab == index} on:click={()=>activeTab = index}>{tab}</button>
+		{#each data.post as doc, index}
+		  <button class="tab" class:tab-active={activeTab == index} on:click={()=>activeTab = index}>{doc.name}</button>
 		{/each}
 	</div>
-	<iframe class="flex-auto h-full" title='legal' src="https://code-crow-legal-docs.s3.amazonaws.com/legal/privacy.pdf"></iframe>
+	<iframe class="flex-auto h-full" title='legal' src={data.post[activeTab].url}></iframe>
 </div>
