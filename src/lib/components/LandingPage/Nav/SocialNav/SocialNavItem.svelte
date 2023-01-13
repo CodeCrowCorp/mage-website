f8c5c8dc06dd32e06e39e364be088669aaf00fff
Gagan Suie <gagan.suie@codecrow.io>
2023-01-13 03:43:27 -0600

    Fix: removed login page and renamed home to browse

--- src/lib/components/LandingPage/Nav/SocialNav/SocialNavItem.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let link: string, image: string;
-</script>
-
-<li class="mx-2.5">
-	<a href={link} target="_blank" rel="noreferrer">
-		<img src={image} alt="Social Link" />
-	</a>
-</li>
--- src/routes/home/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import Carousel from '$lib/components/Home/Carousel.svelte'
-	import ChannelList from '$lib/components/Home/ChannelList.svelte'
-</script>
-
-<Carousel />
-<ChannelList />
