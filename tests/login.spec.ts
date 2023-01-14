e34c99b38912f263800af24da349bef5363bb9f8
Gagan Suie <gagan.suie@codecrow.io>
2023-01-13 19:46:56 -0600

    Fix: resolved playwright tests

--- tests/login.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('login page had expected to load', async ({ page }) => {
-    await page.goto('/login')
-    await expect(page).toHaveURL(/.*login/)
-})
