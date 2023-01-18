255efd385e2d0e89ee4bdcddc7221ec55f8ef136
Kawsar Ahmed <prokawsar@gmail.com>
2023-01-18 07:50:53 -0500

    carousel wip

--- src/lib/components/Browse/Carousel.svelte
+++ src/lib/components/Browse/Carousel.svelte
@@ -1,4 +1,4 @@
-<div class="carousel w-full">
+<!-- <div class="carousel w-full">
 	<div id="slide1" class="carousel-item relative w-full">
 		<img src="https://placeimg.com/800/200/arch" class="w-full" />
 		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -27,4 +27,44 @@
 			<a href="#slide1" class="btn btn-circle">❯</a>
 		</div>
 	</div>
+</div> -->
+<div class="relative">
+	<div class="bg-white rounded-full p-3 absolute top-2/4 left z-10">P</div>
+
+	<div class="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pt-14 flex-grow">
+		<div class="shrink-0 blank-width" />
+		<div class="snap-center shrink-0 first:pl-8 last:pr-8 basis-[600px] rounded-md">
+			<div class="video-thumbnail">Video Thumbnail 1</div>
+		</div>
+		<div class="snap-center shrink-0 first:pl-8 last:pr-8 basis-[600px] rounded-md">
+			<div class="video-thumbnail">Video Thumbnail 2</div>
+		</div>
+		<div class="snap-center shrink-0 first:pl-8 last:pr-8 basis-[600px] rounded-md">
+			<div class="video-thumbnail">Video Thumbnail 3</div>
+		</div>
+		<div class="snap-center shrink-0 first:pl-8 last:pr-8 basis-[600px] rounded-md">
+			<div class="video-thumbnail">Video Thumbnail 4</div>
+		</div>
+
+		<div class="shrink-0 blank-width" />
+	</div>
+
+	<div class="bg-white rounded-full p-3 absolute top-2/4 right">N</div>
 </div>
+
+<style>
+	.video-thumbnail {
+		@apply bg-slate-400 w-full h-80 flex items-center justify-center text-white rounded-md;
+	}
+
+	.right {
+		right: max(30px, (100% - 600px) / 2);
+	}
+	.left {
+		left: max(30px, (100% - 600px) / 2);
+	}
+
+	.blank-width {
+		width: calc((100% - 600px) / 2);
+	}
+</style>
