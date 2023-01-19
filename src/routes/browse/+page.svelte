24f6b2a51248c17a28e37bbab18405f568fcae3c
Kawsar Ahmed <prokawsar@gmail.com>
2023-01-19 01:23:43 -0500

    section added

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -2,6 +2,7 @@
 	import Carousel from '$lib/components/Browse/Carousel.svelte'
 	import ChannelSearch from '$lib/components/Browse/ChannelSearch.svelte'
 	import ChannelList from '$lib/components/Browse/ChannelTable.svelte'
+	import ChannelSection from '$lib/components/Browse/ChannelGroup.svelte'
 	import { onMount } from 'svelte'
 	import { getMyChannels } from '$lib/stores/channelStore'
 
@@ -13,4 +14,8 @@
 
 <Carousel />
 <ChannelSearch />
+<ChannelSection title="Daily category" />
+<ChannelSection title="Most active" />
+<ChannelSection title="Top streamers" />
+
 <ChannelList />
