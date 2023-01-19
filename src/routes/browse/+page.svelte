af615516737ef35885e7a29e32a85c1e217e2c5d
Kawsar Ahmed <prokawsar@gmail.com>
2023-01-19 01:13:48 -0500

    calling method

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -1,6 +1,15 @@
-<script>
+<script lang="ts">
 	import Carousel from '$lib/components/Browse/Carousel.svelte'
 	import ChannelList from '$lib/components/Browse/ChannelList.svelte'
+	import { onMount } from 'svelte'
+	import ChannelStore from '$lib/stores/channelStore'
+
+	const channelStore = new ChannelStore()
+
+	onMount(async () => {
+		let x = await channelStore.getMyChannels()
+		console.log(x)
+	})
 </script>
 
 <Carousel />
