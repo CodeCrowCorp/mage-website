a50a162676922fdeda01a7e214df54c799aa36b4
Kawsar Ahmed <prokawsar@gmail.com>
2023-02-02 06:49:38 -0500

    can be used in load()

--- src/routes/browse/+page.server.ts
+++ src/routes/browse/+page.server.ts
@@ -3,11 +3,9 @@ import type { PageServerLoad } from './$types'
 import { getMyChannels, getChannels } from '$lib/stores/channelStore'
 
 export const load = (async ({ params }) => {
-    // const post = await getChannels()
-
-    // if (post) {
-    //     return post
-    // }
-
-    // throw error(404, 'Not found')
+	// const channels = await getChannels({ skip: 0, limit: 50 })
+	// if (channels) {
+	// 	return { channels }
+	// }
+	// throw error(404, 'Not found')
 }) satisfies PageServerLoad
