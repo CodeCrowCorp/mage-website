12434e80fc499f45a1b5e0930de242f2f5cb651d
Gagan Suie <gagan.suie@codecrow.io>
2023-02-01 18:58:03 -0600

    Feat: added chat message and chat drawer

--- src/lib/components/Chat/ChatDrawer.svelte
+++ src/lib/components/Chat/ChatDrawer.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 	import ChatInput from '$lib/components/Chat/ChatInput.svelte'
+	import ChatMessage from '$lib/components/Chat/ChatMessage.svelte'
 
 	export let showDrawer: boolean
 	export let channel: any = undefined
+
+	let username = 'Gagan',
+		message =
+			'Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World'
+
+	let sender = { username, message }
+
+	let youSender = { username: 'Singh', message: 'hewrewrwe ehe' }
+	let modSender = { username: 'Moderator', message: 'hewe rew rwe rwhe' }
+	let otherSender = { username: 'Randos', message: 'he wer wer wehe' }
 
 	onMount(async () => {
 		// if (!$tags.length) {
@@ -25,7 +36,50 @@
 			<p class="p-3 text-xl mb-5 pb-2 border-purple-500 font-semibold border-b-2">
 				{channel.title || 'Chat'}
 			</p>
-			<div class="flex flex-col p-3" />
+
+			<div class="flex flex-col-reverse p-3 grow h-80 overflow-y-auto">
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={youSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={modSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={otherSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={youSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={modSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={otherSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={youSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={modSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={otherSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={youSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={modSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={otherSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={youSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={modSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={otherSender} />
+				<ChatMessage bind:sender={youSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={modSender} />
+				<ChatMessage bind:sender />
+				<ChatMessage bind:sender={otherSender} />
+			</div>
 
 			<div class="flex flex-row mt-auto p-3 w-full">
 				<ChatInput />
---
+++ src/lib/components/Chat/ChatMessage.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	export let sender: any
+</script>
+
+{#if sender.username == 'Gagan'}
+	<!--Host-->
+	<div class="p-1 hover:btn-ghost border border-transparent rounded-lg flex gap-2 w-96">
+		<span>
+			<span class="bg-secondary rounded-sm text-sm px-[5px] py-[2px] text-white">Host</span>
+			<span class="cursor-pointer text-pink-500 font-medium">@{sender.username}</span>
+			<span>{sender.message}</span>
+		</span>
+	</div>
+{:else if sender.username == 'Singh'}
+	<!--You-->
+	<div
+		class="p-1 hover:btn-ghost border border-transparent rounded-lg flex flex-row-reverse gap-2 w-96">
+		<span>
+			<span>{sender.message}</span>
+			<span class="cursor-pointer font-medium">@{sender.username}</span>
+			<span class="bg-gray-600 rounded-sm text-sm px-[5px] py-[2px] text-white">You</span>
+		</span>
+	</div>
+{:else if sender.username == 'Moderator'}
+	<!--Mod-->
+	<div class="p-1 hover:btn-ghost border border-transparent rounded-lg flex gap-2 w-96">
+		<span>
+			<span class="bg-green-700 rounded-sm text-sm px-[5px] py-[2px] text-white">Mod</span>
+			<span class="cursor-pointer text-success font-medium">@{sender.username}</span>
+			<span>{sender.message}</span>
+		</span>
+	</div>
+{:else}
+	<!--Rando-->
+	<div class="p-1 hover:btn-ghost border border-transparent rounded-lg flex gap-2 w-96">
+		<span>
+			<span class="cursor-pointer text-info font-medium">@{sender.username}</span>
+			<span>{sender.message}</span>
+		</span>
+	</div>
+{/if}
