444bca34deeb8d06ac9fcc2f9342cf7abba89c8b
Gagan Suie <gagan.suie@codecrow.io>
2023-02-24 11:28:13 -0600

    Feat: added view channel information

--- src/lib/components/Channel/Chat/DrawerChat.svelte
+++ src/lib/components/Channel/Chat/DrawerChat.svelte
@@ -3,7 +3,6 @@
 	import Message from '$lib/components/Channel/Chat/Message.svelte'
 	import { channelMessage } from '$lib/stores/websocketStore'
 	import CollapseViewChannel from '$lib/components/Channel/Chat/CollapseViewChannel.svelte'
-	import VideoGrid from '$lib/components/Channel/VideoGrid.svelte'
 
 	export let channel: any = undefined,
 		userId: string = '',
@@ -41,25 +40,14 @@
 	}
 </script>
 
-<div class="drawer drawer-end">
-	<input id="chat-drawer" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content">
-		<VideoGrid />
+<div class="bg-base-100 flex flex-col overflow-y-hidden">
+	<CollapseViewChannel bind:channel />
+	<div class="flex flex-col-reverse p-3 grow overflow-y-auto">
+		{#each chatHistory as sender}
+			<Message bind:sender />
+		{/each}
 	</div>
-	<div class="drawer-side m-5 rounded-lg md:w-fit">
-		<label for="chat-drawer" class="drawer-overlay" />
-
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<div class="bg-base-100 flex flex-col overflow-y-hidden">
-			<CollapseViewChannel bind:channel />
-			<div class="flex flex-col-reverse p-3 grow overflow-y-auto">
-				{#each chatHistory as sender}
-					<Message bind:sender />
-				{/each}
-			</div>
-			<div class="flex flex-row mt-auto p-3 w-full">
-				<ChatInput bind:channelId={channel._id} bind:userId bind:username />
-			</div>
-		</div>
+	<div class="flex flex-row mt-auto p-3 w-full">
+		<ChatInput bind:channelId={channel._id} bind:userId bind:username />
 	</div>
 </div>
