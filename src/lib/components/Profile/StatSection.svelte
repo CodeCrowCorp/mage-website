26b4db484035e24e08d37d9505e4ed01e33f7ddd
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-02 23:54:22 -0500

    moved to stats component

--- src/lib/components/Profile/StatSection.svelte
+++ src/lib/components/Profile/StatSection.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import ListSubscribe from '$lib/components/Profile/ListSubscribe.svelte'
 	import SectionTable from '$lib/components/Browse/Sections/SectionTable.svelte'
+	import Stats from './Elements/Stats.svelte'
 
 	export let myChannels, mySubscribers, isLoading
 
@@ -20,59 +21,7 @@
 				{/each}
 			</div>
 			<div class="grid h-full bg-base-100 py-10" class:hidden={activeTab != 0}>
-				<div class="tooltip" data-tip="level 1">
-					<progress class="progress progress-accent w-64" value="30" max="100" />
-				</div>
-				<div class="stats shadow bg-base-200">
-					<div class="stat">
-						<div class="stat-figure text-primary">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								class="inline-block w-8 h-8 stroke-current"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-						</div>
-						<div class="stat-title">Total Likes</div>
-						<div class="stat-value text-primary">25.6K</div>
-						<div class="stat-desc">21% more than last month</div>
-					</div>
-
-					<div class="stat">
-						<div class="stat-figure text-secondary">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								class="inline-block w-8 h-8 stroke-current"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M13 10V3L4 14h7v7l9-11h-7z" /></svg>
-						</div>
-						<div class="stat-title">Page Views</div>
-						<div class="stat-value text-secondary">2.6M</div>
-						<div class="stat-desc">21% more than last month</div>
-					</div>
-
-					<div class="stat">
-						<div class="stat-figure text-secondary">
-							<div class="avatar online">
-								<div class="w-16 rounded-full">
-									<img src="https://placeimg.com/128/128/people" />
-								</div>
-							</div>
-						</div>
-						<div class="stat-value">86%</div>
-						<div class="stat-title">Tasks done</div>
-						<div class="stat-desc text-secondary">31 tasks remaining</div>
-					</div>
-				</div>
+				<Stats />
 			</div>
 			<div class="flex-auto h-full text-left" class:hidden={activeTab != 1}>
 				<SectionTable bind:channels={myChannels} bind:isLoading />
