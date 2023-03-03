86fb85dcc6f843c3e1622c5383309bb3880baa66
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-03 09:30:31 -0500

    mockup for subscriber

--- src/lib/components/Profile/ListSubscribe.svelte
+++ src/lib/components/Profile/ListSubscribe.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
+	import SubscriberItem from './Elements/SubscriberItem.svelte'
+
 	export let subscribers: any = []
 </script>
 
-<div class="flex flex-row justify-center">Subscribers UI</div>
+<div class="flex flex-row justify-center gap-5 mt-8">
+	<div class="flex flex-col w-2/4 gap-2 bg-blue-200 py-4 px-8">
+		<p class="text-start text-black font-semibold">Following (30)</p>
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+	</div>
+
+	<div class="flex flex-col w-2/4 gap-2 bg-pink-100 py-4 px-8">
+		<p class="text-start text-black font-semibold">Followers (30)</p>
+
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+		<SubscriberItem />
+	</div>
+</div>
