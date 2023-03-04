cee44d8c03fd48b79029c31e3834645121b17c3f
Gagan Suie <gagan.suie@codecrow.io>
2023-03-04 14:46:21 -0600

    Fix: replaced channelStore function with get api function

--- src/lib/components/Channel/VideoGrid.svelte
+++ src/lib/components/Channel/VideoGrid.svelte
@@ -2,14 +2,16 @@
 	import StreamControls from '$lib/components/Channel/StreamControls.svelte'
 </script>
 
-<div class="flex justify-center">
-	<div class="carousel carousel-vertical rounded-lg bg-slate-50 w-full h-96 m-5">
+<div class="flex justify-center h-full">
+	<div class="carousel carousel-vertical rounded-lg bg-base-100 w-full m-5 mb-24">
+		<!-- {#each} -->
 		<div class="carousel-item h-full">
 			<img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" />
 		</div>
 		<div class="carousel-item h-full">
 			<img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" />
 		</div>
+		<!-- {/each} -->
 	</div>
 
 	<!-- <video></video> -->
