7e2b8a91fc753fe67803cd59272093a2bd7c882f
Gagan Suie <gagan.suie@codecrow.io>
2023-03-05 16:05:07 -0600

    Feat: added one-hour timeout for remote config retrieval

--- src/lib/components/Channel/VideoGrid.svelte
+++ src/lib/components/Channel/VideoGrid.svelte
@@ -5,12 +5,12 @@
 <div class="flex justify-center h-full">
 	<div class="carousel carousel-vertical rounded-lg bg-base-100 w-full m-5 mb-24">
 		<!-- {#each} -->
-		<div class="carousel-item h-full">
+		<!-- <div class="carousel-item h-full">
 			<img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" />
 		</div>
 		<div class="carousel-item h-full">
 			<img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" />
-		</div>
+		</div> -->
 		<!-- {/each} -->
 	</div>
 
