8d21580205d62dcadb5b36c1d1b0e15d83d15b4b
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-06 23:47:00 -0500

    seperate component

--- src/routes/profile/[customUsername]/+page.svelte
+++ src/routes/profile/[customUsername]/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import IconLink from '$lib/assets/icons/IconLink.svelte'
 	import { onMount } from 'svelte'
 	import { page } from '$app/stores'
 	import DrawerEditProfile from '$lib/components/Profile/DrawerEditProfile.svelte'
 	import { get } from '$lib/api'
-	import NameLoader from '$lib/components/Profile/Elements/NameLoader.svelte'
-	import CategoryLoader from '$lib/components/Profile/Elements/CategoryLoader.svelte'
 	import { getTechListJson, techList } from '$lib/stores/channelStore'
 	import StatSection from '$lib/components/Profile/StatSection.svelte'
 	import TopSection from '$lib/components/Profile/TopSection.svelte'
+	import UserDetails from '$lib/components/Profile/Elements/UserDetails.svelte'
 
 	export let data
 	$: ({ profile } = data)
@@ -77,49 +75,7 @@
 			class="relative flex flex-col min-w-0 break-words bg-base-100 w-full mb-6 shadow-xl rounded-lg -mt-64">
 			<div class="px-6">
 				<TopSection bind:profile bind:showDrawer />
-				<div class="text-center flex flex-col items-center">
-					{#if profile?.displayName}
-						<h3 class="text-4xl font-semibold leading-normal mb-2">
-							{profile?.displayName || ''}
-						</h3>
-					{:else}
-						<NameLoader />
-					{/if}
-					{#if profile?.username}
-						<div class="text-lg leading-normal mt-0 mb-2 font-bold text-pink-500">
-							@{profile?.username || ''}
-						</div>
-					{:else}
-						<NameLoader />
-					{/if}
-					{#if profile?.bio || profile?.description}
-						<div class="text-lg leading-normal mt-0 mb-2">
-							{profile?.bio || profile?.description || ''}
-						</div>
-					{/if}
-					<div class="pt-4">
-						{#if profile?.html_url}
-							<div class="flex gap-2 justify-center p-4">
-								<IconLink />
-								<a class="link link-info" href={profile?.html_url} target="_blank" rel="noreferrer"
-									>{profile?.html_url || ''}</a>
-							</div>
-						{/if}
-						<div class="flex gap-2 justify-center">
-							{#if profile?.category}
-								{#if profile?.category.length}
-									{#each profile.category as category}
-										<div class="tooltip" data-tip={category}>
-											<img src={$techList[category]} alt="" class="h-7 w-7 m-1" />
-										</div>
-									{/each}
-								{:else}
-									<CategoryLoader />
-								{/if}
-							{/if}
-						</div>
-					</div>
-				</div>
+				<UserDetails bind:profile bind:techList={$techList} />
 				<StatSection
 					bind:myChannels={data.lazy.myChannels}
 					bind:mySubscribers={data.lazy.mySubscribers} />
