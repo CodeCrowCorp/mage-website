e43ce978dc27e6d5b199e51e763f202aa7750e34
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-24 16:36:47 +0600

    rounded

--- src/lib/components/Channel/Stream/VideoItem.svelte
+++ src/lib/components/Channel/Stream/VideoItem.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="w-96">
-	<div class="bg-gray-200 relative w-full h-full">
+	<div class="bg-gray-200 relative w-full h-full rounded-md">
 		<div class="flex items-center justify-center h-full">
 			<div class="relative w-14 h-14 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
 				<svg
