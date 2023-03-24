bcdba254648e8b78545c3f5bc10e638a91e1ed85
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-24 15:36:17 +0600

    videoItem

--- src/lib/components/Channel/Stream/VideoItem.svelte
+++ src/lib/components/Channel/Stream/VideoItem.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 </script>
 
-<div class="card w-full bg-base-100 shadow-xl border border-red-300">
-	<div class="card-body">
-		<h2 class="card-title">Shoes!</h2>
-		<p>If a dog chews shoes whose shoes does he choose?</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Buy Now</button>
+<div class="w-full">
+	<div class="bg-gray-200 relative w-full h-full">
+		<div class="flex items-center justify-center h-full">
+			<div class="relative w-14 h-14 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+				<svg
+					class="absolute w-16 h-16 text-gray-400 -left-1"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+						clip-rule="evenodd" /></svg>
+			</div>
+		</div>
+		<div class="absolute left-2 bottom-1">
+			<span class="text-sm">@Username</span>
 		</div>
 	</div>
 </div>
