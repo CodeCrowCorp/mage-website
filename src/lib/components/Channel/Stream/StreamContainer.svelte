dc6fc73e072a16a313d7c3e10126f5f3306ceba9
Gagan Suie <gagan.suie@codecrow.io>
2023-04-16 00:59:39 -0500

    Fix: websocket channel chat and added banning users

--- src/lib/components/Channel/Stream/StreamContainer.svelte
+++ src/lib/components/Channel/Stream/StreamContainer.svelte
@@ -6,10 +6,10 @@
 	import { goto } from '$app/navigation'
 	import { page } from '$app/stores'
 	import { createEventDispatcher } from 'svelte'
-	import { is_chat_drawer_open } from '$lib/stores/channelStore'
+	import { video_items } from '$lib/stores/streamStore'
 
 	const dispatch = createEventDispatcher()
-	export let count: number = 10,
+	export let userCount: number = 10,
 		channel: any,
 		channels: any = []
 
@@ -22,7 +22,7 @@
 			if (entry) {
 				const id = entry.target.id
 				channel = channels?.filter((channel: any) => channel?._id === id)[0]
-				if (channels?.length && channels[channels.length - 2]['_id'] == id) {
+				if (channels?.length && channels[channels.length - 2]?._id == id) {
 					dispatch('loadMore')
 				}
 
@@ -40,40 +40,14 @@
 
 <div class="flex justify-center h-full">
 	<div class="carousel carousel-vertical rounded-lg bg-base-100 w-full m-5 mb-24">
-		<!-- <div
-			class="carousel-item h-full p-3 {$is_chat_drawer_open ? 'w-[70%]' : ''}"
-			id={channel?._id}
-			use:autoActive>
-			<div class="flex flex-col w-full">
-				<div class="flex gap-2 mb-3">
-					<span
-						class="btn-sm bg-red-700 rounded-md font-medium text-white border-none flex items-center">
-						LIVE
-					</span>
-					<div class="dropdown dropdown-bottom">
-						<label
-							for=""
-							class="btn btn-sm rounded-md font-medium gap-2 text-white border-none"
-							tabindex="-1">
-							<IconViewers />
-							{count}
-						</label>
-						<DropdownViewers />
-					</div>
-				</div>
-				<VideoGrid />
-			</div>
-		</div> -->
-
 		{#each channels as nextchannel}
-			<div
-				class="carousel-item h-full p-3 {$is_chat_drawer_open ? 'w-[70%]' : ''}"
-				id={nextchannel?._id}
-				use:autoActive>
+			<div class="carousel-item h-full p-3" id={nextchannel?._id} use:autoActive>
 				<div class="flex flex-col w-full">
 					<div class="flex gap-2 mb-3">
 						<span
-							class="btn-sm bg-red-700 rounded-md font-medium text-white border-none flex items-center">
+							class="btn btn-sm rounded-md font-medium text-white border-none flex items-center {$video_items.length
+								? 'bg-red-700 hover:bg-red-700'
+								: ''}">
 							LIVE
 						</span>
 						<div class="dropdown dropdown-bottom">
@@ -82,12 +56,12 @@
 								class="btn btn-sm rounded-md font-medium gap-2 text-white border-none"
 								tabindex="-1">
 								<IconViewers />
-								{count}
+								{userCount}
 							</label>
 							<DropdownViewers />
 						</div>
 					</div>
-					<VideoGrid />
+					<VideoGrid {channel} />
 				</div>
 			</div>
 		{/each}
