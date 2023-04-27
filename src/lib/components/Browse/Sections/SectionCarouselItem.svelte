1e949a35b94fc1ea7e18c04114b7c8c880d94955
kahmed-at-979455188925 <prokawsar@gmail.com>
2023-04-27 17:52:17 +0600

    fix bg cover

--- src/lib/components/Browse/Sections/SectionCarouselItem.svelte
+++ src/lib/components/Browse/Sections/SectionCarouselItem.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	export let channel: any = {}
+
+	let bg_cover = channel.thumbnail || '/src/lib/assets/placeholder/programming-placeholder.jpg'
 </script>
 
-<a class="swiper-slide bg-slate-50 !flex cursor-pointer rounded-md" href="/channel/{channel._id}">
-	<div class="flex flex-row items-end w-full">
+<a class="swiper-slide bg-slate-50 cursor-pointer rounded-md" href="/channel/{channel._id}">
+	<div class="flex flex-row items-end w-full h-full" style="background-image: url({bg_cover});">
 		<div class="flex flex-row items-center gap-2 ml-2 mb-2">
 			<div class="avatar">
 				<div class="w-14 mask mask-squircle">
@@ -12,7 +14,9 @@
 			</div>
 			<div class="w-full">
 				<p class="text-2xl truncate font-semibold">{channel.title || ''}</p>
-				<p class="truncate">@{channel.username}</p>
+				<p class="truncate">
+					{channel.username ? '@' + channel.username : channel.displayName || ''}
+				</p>
 			</div>
 		</div>
 	</div>
@@ -20,6 +24,6 @@
 
 <style>
 	.video-thumbnail {
-		@apply w-full h-64 flex items-center justify-center mb-2;
+		@apply w-full h-full;
 	}
 </style>
