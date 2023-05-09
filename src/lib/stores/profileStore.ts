88b5ddce721f887c452d2d5374c512ed34ba39cf
Kawsar Ahmed <prokawsar@gmail.com>
2023-05-09 16:43:51 +0600

    removed unused

--- src/lib/stores/profileStore.ts
+++ src/lib/stores/profileStore.ts
@@ -2,4 +2,3 @@ import { writable, type Writable } from 'svelte/store'
 
 export const subscriber_count: Writable<number> = writable(0)
 export const interest_count: Writable<number> = writable(0)
-export const app_theme: Writable<string> = writable('light')
