2172a34783da30d926d43de07dac4a0fa4acc283
Kawsar Ahmed <prokawsar@gmail.com>
2023-05-12 11:06:23 +0600

    fix carosuel item UI

--- src/lib/components/Browse/Sections/SectionCarouselItem.svelte
+++ src/lib/components/Browse/Sections/SectionCarouselItem.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import IconViewers from '$lib/assets/icons/IconViewers.svelte'
+
 	export let channel: any = {}
 
 	let bg_cover = channel.thumbnail || '/src/lib/assets/placeholder/programming-placeholder.jpg'
@@ -8,16 +10,31 @@
 	class="swiper-slide bg-slate-50 cursor-pointer rounded-md"
 	href="/channel/{channel._id}"
 	style="background-image: url({bg_cover});">
-	<div class="flex flex-row items-end w-full h-full">
-		<div class="flex flex-row items-center gap-2 ml-2 mb-2">
+	<div class="flex flex-col justify-between p-3 w-full h-full">
+		<div class="flex gap-2 mb-3">
+			<span
+				class="btn btn-sm rounded-md font-medium text-white border-none flex items-center bg-red-700 hover:bg-red-700">
+				LIVE
+			</span>
+			<div class="dropdown dropdown-bottom">
+				<label
+					for=""
+					class="btn btn-sm rounded-md font-medium gap-2 text-white border-none"
+					tabindex="-1">
+					<IconViewers /> 10
+				</label>
+			</div>
+		</div>
+
+		<div class="flex flex-row items-center gap-2">
 			<div class="avatar">
 				<div class="w-14 mask mask-squircle">
 					<img src={channel.avatar} alt="" />
 				</div>
 			</div>
 			<div class="w-full">
-				<p class="text-2xl truncate font-semibold">{channel.title || ''}</p>
-				<p class="truncate">
+				<p class="text-2xl truncate text-white font-semibold">{channel.title || ''}</p>
+				<p class="truncate text-white">
 					{channel.username ? '@' + channel.username : channel.displayName || ''}
 				</p>
 			</div>
