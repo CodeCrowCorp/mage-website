25e811bb417390b9675a33dadc156c8a4aeed4f1
Gagan Suie <gagan.suie@codecrow.io>
2023-05-17 02:47:23 -0500

    Feat: updated the LoadingItemSearch layout

--- src/lib/components/Search/LoadingItemSearch.svelte
+++ src/lib/components/Search/LoadingItemSearch.svelte
@@ -5,20 +5,33 @@
 
 <div
 	role="status"
-	class="max-w-sm p-4 border border-gray-200 rounded shadow md:p-6 dark:border-gray-700 mx-3">
-	<div class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
-		<IconBrowseItemPlay />
-	</div>
-	<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4" />
-	<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
-	<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
-	<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
-	<div class="flex items-center mt-4 space-x-3">
-		<IconBrowseItemUser />
-		<div>
-			<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2" />
-			<div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+	class="w-full p-4 border border-gray-200 rounded-md shadow md:p-6 dark:border-gray-700">
+	<div class="flex flex-col md:flex-row gap-4 w-full">
+		<div class="min-w-[20rem]">
+			<div class="flex items-center justify-center h-64 bg-gray-300 rounded-md dark:bg-gray-700">
+				<IconBrowseItemPlay />
+			</div>
+		</div>
+		<div class="flex-auto flex flex-col gap-2">
+			<div class="flex gap-2 mb-2.5">
+				<div class="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-8" />
+				<div class="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-8" />
+				<div class="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-8" />
+				<div class="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-8" />
+			</div>
+			<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-2.5" />
+			<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
+			<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-1/2" />
+			<div class="flex items-center space-x-3">
+				<IconBrowseItemUser />
+				<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32" />
+			</div>
+			<div class="flex gap-2 mb-2.5">
+				<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-16" />
+				<div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-16" />
+			</div>
+			<div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-32" />
+			<span class="sr-only">Loading...</span>
 		</div>
 	</div>
-	<span class="sr-only">Loading...</span>
 </div>
