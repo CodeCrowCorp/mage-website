3298da2ebf8fa35f5aecf56a701aa41807fc483d
Gagan Suie <gagan.suie@codecrow.io>
2023-05-30 02:15:36 -0500

    Feat: changed placeholder content to slash commands list

---
+++ src/lib/components/Channel/Stream/CommandList.svelte
@@ -0,0 +1,16 @@
+<div
+	class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all animate-pulse">
+	<span class="flex items-center justify-center mb-4">Coming soon...</span>
+	<div class="grid grid-cols-2 gap-2">
+		<div class="grid grid-rows-3 gap-y-4">
+			<div class="flex items-center justify-end">Toggle mod status</div>
+			<div class="flex items-center justify-end">Toggle guest status</div>
+			<div class="flex items-center justify-end">Ban user</div>
+		</div>
+		<div class="grid grid-rows-3 gap-y-4">
+			<div class="text-left"><kbd class="text-left kbd">/mod @username</kbd></div>
+			<div class="text-left"><kbd class="kbd">/guest @username</kbd></div>
+			<div class="text-left"><kbd class="text-left kbd">/ban @username</kbd></div>
+		</div>
+	</div>
+</div>
