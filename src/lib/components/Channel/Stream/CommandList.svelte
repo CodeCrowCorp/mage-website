7a579dffdded75e08fe4dc1633087c0b882abc43
Gagan Suie <gagan.suie@codecrow.io>
2023-05-30 03:28:00 -0500

    Fix: added description for slash commands

--- src/lib/components/Channel/Stream/CommandList.svelte
+++ src/lib/components/Channel/Stream/CommandList.svelte
@@ -1,6 +1,6 @@
 <div
 	class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all animate-pulse">
-	<span class="flex items-center justify-center mb-4">Coming soon...</span>
+	<span class="flex items-center justify-center mb-4">Slash commands coming soon...</span>
 	<div class="grid grid-cols-2 gap-2">
 		<div class="grid grid-rows-3 gap-y-4">
 			<div class="flex items-center justify-end">Toggle mod status</div>
