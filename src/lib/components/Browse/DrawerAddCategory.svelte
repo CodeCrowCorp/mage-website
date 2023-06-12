0211594e0a86d8d9d6998bb3e8a2c8a937defe62
Gagan Suie <gagan.suie@codecrow.io>
2023-06-12 16:58:44 -0500

    Fix: minor padding changes

--- src/lib/components/Browse/DrawerAddCategory.svelte
+++ src/lib/components/Browse/DrawerAddCategory.svelte
@@ -88,78 +88,80 @@
 	})
 </script>
 
-<div class="bg-base-200 rounded-lg w-80 md:w-[30rem] flex flex-col {classes} h-full">
-	<p class="p-3 text-xl mb-5 pb-2 border-purple-500 font-semibold border-b-2">Select category</p>
-	<div class="flex flex-col p-3 h-full">
-		<div class="relative">
-			<div class="flex gap-1 input input-primary">
-				<div class="flex flex-row gap-2 items-center left-0">
-					{#if categories?.length}
-						{#each categories as icon}
-							<img
-								src={$category_list[icon]}
-								alt=""
-								class="h-5 w-5 cursor-pointer"
-								on:click={() => removeCategory(icon)} />
-						{/each}
-					{/if}
-				</div>
+<div class="flex h-full p-5">
+	<div class="bg-base-200 rounded-lg w-80 md:w-[30rem] flex flex-col {classes}">
+		<p class="p-3 text-xl mb-5 pb-2 border-purple-500 font-semibold border-b-2">Select category</p>
+		<div class="flex flex-col p-3 h-full">
+			<div class="relative">
+				<div class="flex gap-1 input input-primary">
+					<div class="flex flex-row gap-2 items-center left-0">
+						{#if categories?.length}
+							{#each categories as icon}
+								<img
+									src={$category_list[icon]}
+									alt=""
+									class="h-5 w-5 cursor-pointer"
+									on:click={() => removeCategory(icon)} />
+							{/each}
+						{/if}
+					</div>
 
-				<input
-					type="text"
-					bind:value={searchQuery}
-					on:input={() => searchCategory()}
-					name=""
-					class="grow md:ml-4 md:mr-12 focus:outline-0 max-w-[8rem] bg-base-100 md:max-w-xs"
-					placeholder={categories?.length ? '' : 'Category'}
-					autocomplete="off" />
+					<input
+						type="text"
+						bind:value={searchQuery}
+						on:input={() => searchCategory()}
+						name=""
+						class="grow md:ml-4 md:mr-12 focus:outline-0 max-w-[8rem] bg-base-100 md:max-w-xs"
+						placeholder={categories?.length ? '' : 'Category'}
+						autocomplete="off" />
+				</div>
+				<span class="absolute right-0 top-1/4 text-gray-400 pr-3">({maxCategoryLabel})</span>
 			</div>
-			<span class="absolute right-0 top-1/4 text-gray-400 pr-3">({maxCategoryLabel})</span>
-		</div>
 
-		<div class="tabs tabs-boxed mt-5">
-			{#each tabs as tab}
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<span class="tab" class:tab-active={activeTab == tab} on:click={() => setActiveTab(tab)}
-					>{tab}</span>
-			{/each}
-		</div>
-
-		<div class="flex flex-col grow h-80 overflow-y-scroll mt-5">
-			{#if renderingAssets.length}
-				{#each renderingAssets as [name, image_url]}
+			<div class="tabs tabs-boxed mt-5">
+				{#each tabs as tab}
 					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<label
-						class="cursor-pointer flex items-center gap-2 pb-2"
-						on:click={() => toggleCategory(name, image_url)}>
-						<input
-							type="checkbox"
-							checked={categories.includes(name)}
-							class="checkbox checkbox-primary"
-							disabled={categories.length === maxCategory && !categories.includes(name)} />
-						<img src={image_url} alt="" class="h-7 w-7 m-1" />
-						<span class="label-text">{name}</span>
-					</label>
+					<span class="tab" class:tab-active={activeTab == tab} on:click={() => setActiveTab(tab)}
+						>{tab}</span>
 				{/each}
-			{:else if searchQuery != ''}
-				<div class="alert flex justify-center">
-					<IconInfo />
-					<p>No results for the search query</p>
-				</div>
-			{:else}
-				<div class="flex justify-center w-full">
-					<progress class="progress w-full" />
-				</div>
-			{/if}
-		</div>
-	</div>
+			</div>
 
-	<div class="flex flex-row mt-auto gap-2 md:mb-4 p-3">
-		<button
-			type="button"
-			class="btn btn-default grow"
-			on:click={() => ((categories = []), (showAddCategory = false))}>Cancel</button>
-		<button type="button" class="btn btn-primary grow" on:click={() => (showAddCategory = false)}
-			>Add</button>
+			<div class="flex flex-col grow h-80 overflow-y-scroll mt-5">
+				{#if renderingAssets.length}
+					{#each renderingAssets as [name, image_url]}
+						<!-- svelte-ignore a11y-click-events-have-key-events -->
+						<label
+							class="cursor-pointer flex items-center gap-2 pb-2"
+							on:click={() => toggleCategory(name, image_url)}>
+							<input
+								type="checkbox"
+								checked={categories.includes(name)}
+								class="checkbox checkbox-primary"
+								disabled={categories.length === maxCategory && !categories.includes(name)} />
+							<img src={image_url} alt="" class="h-7 w-7 m-1" />
+							<span class="label-text">{name}</span>
+						</label>
+					{/each}
+				{:else if searchQuery != ''}
+					<div class="alert flex justify-center">
+						<IconInfo />
+						<p>No results for the search query</p>
+					</div>
+				{:else}
+					<div class="flex justify-center w-full">
+						<progress class="progress w-full" />
+					</div>
+				{/if}
+			</div>
+		</div>
+
+		<div class="flex flex-row mt-auto gap-2 md:mb-4 p-3">
+			<button
+				type="button"
+				class="btn btn-neutral text-white grow"
+				on:click={() => ((categories = []), (showAddCategory = false))}>Cancel</button>
+			<button type="button" class="btn btn-primary grow" on:click={() => (showAddCategory = false)}
+				>Add</button>
+		</div>
 	</div>
 </div>
