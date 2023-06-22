ac279e8bcb51bc387b447b6a34a233aeb8ad416a
Gagan Suie <gagan.suie@codecrow.io>
2023-06-22 11:40:35 -0500

    Fix: numbered code snippet

--- src/lib/components/Channel/Chat/Message.svelte
+++ src/lib/components/Channel/Chat/Message.svelte
@@ -7,7 +7,7 @@
 	import IconChatHorizontalMore from '$lib/assets/icons/chat/IconChatHorizontalMore.svelte'
 	import ProfileCard from '$lib/components/Channel/Chat/ProfileCard.svelte'
 	import { emitChannelUpdate, emitDeleteMessageToChannel } from '$lib/websocket'
-	import { getColoredRole, setRole } from '$lib/utils'
+	import { copyToClipboard, getColoredRole, setRole } from '$lib/utils'
 	import { page } from '$app/stores'
 	import IconChatBan from '$lib/assets/icons/chat/IconChatBan.svelte'
 
@@ -102,33 +102,34 @@
 	}
 
 	const isCodeSnippet = (message: string = '') => {
-		if(message.includes("`")){
+		if (message.includes('`')) {
 			return true
 		}
 	}
 
 	const getCodeSnippet = (message: string = '') => {
-		if(message.includes("`")){
-			const s = message.indexOf("`");
-			const e = message.lastIndexOf("`")
-			
+		if (message.includes('`')) {
+			const s = message.indexOf('`')
+			const e = message.lastIndexOf('`')
+
 			let code = message.substring(s, e)
 			let splitted = message.split(code)
-			let resp:any = {}
+			let resp: any = {}
 			resp.startText = splitted[0]
-			resp.code = code.substr(1).split(";").map(i => i.trim() + ";")
-			resp.endText = splitted.pop()?.substr(1) || ""
+			resp.code = code
+				.substr(1)
+				.split('\n')
+				.map((i) => i)
+			resp.endText = splitted.pop()?.substr(1) || ''
 
 			return resp
 		}
 	}
 
 	$: codeSnippet = isCodeSnippet(sender.message) ? getCodeSnippet(sender.message) : false
-	$: console.log("codeSnippet: ", codeSnippet)
-
 </script>
 
-<ul class="menu">
+<ul class="menu" on:click={() => copyToClipboard(sender.message)}>
 	<li class="group relative dropdown">
 		<!--Host, Mod, You or Rando-->
 		<div class="p-1 border border-transparent rounded-lg flex gap-2 overflow-x-hidden">
@@ -150,11 +151,11 @@
 						<span class="break-all">{codeSnippet.startText}</span>
 					{/if}
 					{#if codeSnippet.code}
-					  <div class="mockup-code my-2">
-						{#each codeSnippet.code as line, idx}
-							<pre data-prefix={idx + 1}><code>{line}</code></pre> 
-						{/each}
-					  </div>
+						<div class="mockup-code my-2">
+							{#each codeSnippet.code as line, idx}
+								<pre data-prefix={idx + 1}><code>{line}</code></pre>
+							{/each}
+						</div>
 					{/if}
 					{#if codeSnippet.endText}
 						<span class="break-all">{codeSnippet.endText}</span>
