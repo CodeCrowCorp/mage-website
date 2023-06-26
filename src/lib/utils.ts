8fd056407333655a9a64fedff341d1262accd16c
sitara-dev <sw.sitara@gmail.com>
2023-06-26 13:27:43 +0530

    fixed username validation issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { browser } from "$app/environment";
+
 export const levelAndBarValueFromExp = (
 	currentExp: number
 ): { level: number; barValue: number } => {
@@ -269,3 +271,19 @@ export const clickOutside = (element: any, callbackFunction: any) => {
 		}
 	}
 }
+
+
+export const createEffect = (...initialDeps: any[]) => {
+	let diff = JSON.stringify(initialDeps);
+	return (callback: () => void, deps?: any[], allowServerSide = false) => {
+	  if (
+		JSON.stringify(deps) !== diff &&
+		((!allowServerSide && browser) || allowServerSide)
+	  ) {
+		diff = JSON.stringify(deps);
+		callback();
+	  } else if (!deps) {
+		diff = "";
+	  }
+	};
+};
