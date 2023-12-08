80e0e935f396cdc77200493d03e011f41930d4b2
Gagan Suie <gagan.suie@codecrow.io>
2023-12-08 02:01:02 -0600

    Fix: renaming feature flags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 		is_feature_video_responses_enabled,
 		is_feature_premium_enabled,
 		is_feature_apps_enabled,
-		is_feature_studio_mode_enabled
+		is_feature_merge_platforms_enabled
 	} from '$lib/stores/remoteConfigStore'
 	import { env } from '$env/dynamic/public'
 	import { user_role } from '$lib/stores/userStore'
@@ -50,7 +50,7 @@
 		$is_feature_video_responses_enabled = env.PUBLIC_FEATURE_VIDEO_RESPONSES === 'true'
 		$is_feature_premium_enabled = env.PUBLIC_FEATURE_PREMIUM === 'true'
 		$is_feature_apps_enabled = env.PUBLIC_FEATURE_APPS === 'true'
-		$is_feature_studio_mode_enabled = env.PUBLIC_FEATURE_STUDIO_MODE === 'true'
+		$is_feature_merge_platforms_enabled = env.PUBLIC_FEATURE_MERGE_PLATFORMS === 'true'
 		await handleWebsocket()
 		if (!$category_list.length) {
 			$category_list = imageUrlsJson
