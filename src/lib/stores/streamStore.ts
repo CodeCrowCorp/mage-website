bd1d6820310bcfa916fc1dcd08b287b343059fc8
Gagan Suie <gagan.suie@codecrow.io>
2023-12-22 04:06:23 -0600

    Fix: removed unused is_wip_webrtc flag and libraries

--- src/lib/stores/streamStore.ts
+++ src/lib/stores/streamStore.ts
@@ -1,7 +1,5 @@
 import { writable, type Writable } from 'svelte/store'
 
-export const is_wip_webrtc: Writable<boolean | undefined> = writable(false)
-
 export const is_sharing_rtmps: Writable<boolean | undefined> = writable(false)
 export const is_sharing_screen: Writable<boolean | undefined> = writable(undefined)
 export const is_sharing_webcam: Writable<boolean | undefined> = writable(undefined)
