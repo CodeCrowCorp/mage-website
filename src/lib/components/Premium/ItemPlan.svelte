d04b9548de49dd98af0694b615e7cc0fee2ae624
Gagan Suie <gagan.suie@codecrow.io>
2024-01-31 23:26:15 -0600

    Feat: integrated getPlans endpoint

--- src/lib/components/Premium/ItemPlan.svelte
+++ src/lib/components/Premium/ItemPlan.svelte
@@ -1,62 +1,63 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 	import IconAffiliateCheck from '$lib/assets/icons/premium/IconAffiliateCheck.svelte'
-	import IconAffiliateVerification from '$lib/assets/icons/premium/IconAffiliateVerification.svelte'
+	import { onMount } from 'svelte'
 
-	export let plan: any = {}
+	export let plan: any = {},
+		index: number = 0,
+		recurringInterval: string = 'month'
+
+	$: if (recurringInterval) {
+		price = plan.prices.find((price: any) => price.recurring.interval === recurringInterval)
+	}
+
+	let price: any
+	onMount(() => {
+		console.log('recurringInterval', recurringInterval)
+	})
 </script>
 
 <div
-	class="card max-w-sm flex flex-col p-6 mx-auto text-center border border-gray-100 shadow dark:border-gray-600 xl:p-8">
-	<h3 class="text-2xl {plan.name === 'Premium' ? 'text-accent font-bold' : 'font-semibold'}">
-		{plan.name}
-	</h3>
-	<!-- <p class=" text-gray-500 sm:text-lg">
-		{plan.description}
-	</p> -->
+	class="card max-w-sm flex flex-col p-6 mx-auto text-center xl:p-8 border border-gray-100 shadow dark:border-gray-600">
+	{#if index > 1}
+		<div class="bg-accent rounded-lg">
+			<h3 class="text-2xl text-accent-content font-bold">
+				{plan.name}
+			</h3>
+		</div>
+	{:else}
+		<h3 class="text-2xl {index > 0 ? 'text-accent font-bold' : 'font-semibold'}">
+			{plan.name}
+		</h3>
+	{/if}
 	<div class="flex justify-center items-baseline my-8">
-		<span class="mr-2 text-5xl font-extrabold">${plan.price}</span>
-		<span class="text-gray-500">/month</span>
+		<span class="mr-2 text-5xl font-extrabold">${price?.unit_amount / 100}</span>
+		<span class="text-gray-500">/{price?.recurring?.interval}</span>
 	</div>
+
+	<p class="sm:text-sm mb-8 font-semibold">
+		{plan.description}
+	</p>
+
 	<ul class="mb-8 space-y-4 text-left">
 		{#each plan.features as feature}
 			<li class="flex items-center space-x-3">
 				<IconAffiliateCheck />
-				<span>{feature}</span>
+				<span>{feature.name}</span>
 			</li>
 		{/each}
-
-		{#if plan.features2.length}
-			<div class="divider text-accent font-bold link link-accent">Requires Affiliate</div>
-			{#each plan.features2 as feature}
-				<li class="flex items-center space-x-3">
-					<IconAffiliateVerification />
-					<span>{feature}</span>
-				</li>
-			{/each}
-		{/if}
 	</ul>
 	{#if $page.data.user?.userId}
-		<form>
+		<form style="margin-top: auto;">
 			<div class="flex flex-col space-y-3">
-				{#if plan.name === 'Premium'}
-					<button
-						class="btn btn-accent px-5 py-2.5 text-black"
-						disabled={$page.data.user?.user?.planTier > 0}
-						>{$page.data.user?.user?.planTier > 0 ? 'Selected' : 'Select'}</button>
-					{#if $page.data.user?.user?.planTier === 1}
-						<a
-							href="https://forms.gle/mBtByR6jdoJeQd367"
-							class="link link-accent link-hover font-bold">
-							Apply for verification
-						</a>
-					{/if}
-				{:else}
-					<button
+				<button
+					class="btn btn-accent px-5 py-2.5 text-black"
+					disabled={$page.data.user?.user?.planTier > 0}
+					>{$page.data.user?.user?.planTier > 0 ? 'Selected' : 'Select'}</button>
+				<!-- <button
 						class="btn btn-neutral px-5 py-2.5"
 						disabled={$page.data.user?.user?.planTier === 0}
-						>{$page.data.user?.user?.planTier === 0 ? 'Selected' : 'Select'}</button>
-				{/if}
+						>{$page.data.user?.user?.planTier === 0 ? 'Selected' : 'Select'}</button> -->
 			</div>
 		</form>
 	{/if}
