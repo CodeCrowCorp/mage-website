e74f66180e62238b44bdec05d9bc6dcaa2fdb400
Gagan Suie <gagan.suie@codecrow.io>
2024-02-11 03:15:23 -0600

    Fix: file name extra chars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -68,7 +68,7 @@
 		modules.forEach((module: any) => {
 			let filename = module.default.split('/').pop() || ''
 			filename = filename
-				.replace('.svg', '')
+				.replace(/\.[A-Za-z0-9]*\.svg$/, '')
 				.replace(/-/g, ' ')
 				.replace(/\w\S*/g, (w: any) => w.replace(/^\w/, (c: any) => c.toUpperCase()))
 			$category_list[filename] = module.default
