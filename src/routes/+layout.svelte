996f06217f227101f9c34dfb08572ed29859986b
Gagan Suie <gagan.suie@codecrow.io>
2024-02-11 11:50:24 -0600

    Fix: testing file renaming

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -65,7 +65,9 @@
 
 	const processSvgs = (svgs: any, category: any) => {
 		const modules = Object.values(svgs)
+		console.log('modules', modules)
 		modules.forEach((module: any) => {
+			console.log('module', module)
 			let filename = module.default.split('/').pop() || ''
 			// filename = filename
 			// 	.replace(/\..*$/, '')
