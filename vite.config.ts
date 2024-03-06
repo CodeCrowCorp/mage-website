fbabbf6d71c53bc9d00d016e5e875fb10fe063d8
Gagan Suie <gagan.suie@codecrow.io>
2024-03-05 23:26:47 -0600

    Fix: removed sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
-    plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "code-crow-corp",
-            project: "mage-website"
-        }
-    }), sveltekit()],
-
-    define: {
+	plugins: [sveltekit()],
+	define: {
 		__VERSION__: JSON.stringify(process.env.npm_package_version)
 	},
-
-    build: {
+	build: {
 		assetsInlineLimit: 0
 	}
 }
