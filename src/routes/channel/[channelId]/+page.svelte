b717cd0f9d2ea873f7e0fe677d4239ef7d67943d
Gagan Suie <gagan.suie@codecrow.io>
2024-03-06 00:57:23 -0600

    Fix: removed logs

--- src/routes/channel/[channelId]/+page.svelte
+++ src/routes/channel/[channelId]/+page.svelte
@@ -194,19 +194,15 @@
 				})
 			}, 5000)
 			const hasSponsors = $page.url?.searchParams?.get('hasSponsors') || ''
-			const hasSponsors2 = $page.url?.searchParams?.has('hasSponsors')
-			console.log('got here---hasSponsors1', hasSponsors)
-			console.log('got here---hasSponsors2', hasSponsors2)
 			if (hasSponsors) {
-				console.log('got here---hasSponsors3', hasSponsors)
 				emitGetSponsors({
 					channelSocket: chan.socket,
 					recipientUserId: chan.userId,
 					channelId: chan._id
 				})
-				// goto(`/channel/${$page.params.channelId}`, {
-				// 	replaceState: true
-				// })
+				goto(`/channel/${$page.params.channelId}`, {
+					replaceState: true
+				})
 			}
 		}
 	}
